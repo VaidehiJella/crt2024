93016fa7599a5314d08722b9995a9de54bd162dc
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-06-21 13:20:33 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sqr
+{
+ public static void main(String args[])
+ {
+   Scanner s=new Scanner(System.in);
+   int a,c;
+   a=s.nextInt();
+   c=a*a;
+   System.out.println(c);
+ }
+}
