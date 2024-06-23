f39576e6ac0cf7692d4a9ce9d69136c315851820
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-06-23 21:43:25 +0530

    Add files via upload

---
+++ Pow.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Pow
+{
+public static void main(String args[])
+{
+ Scanner s=new Scanner(System.in);
+ int m=s.nextInt();
+ int n=s.nextInt();
+ double b=Math.pow(m,n);
+ System.out.println(b);
+}
+}
