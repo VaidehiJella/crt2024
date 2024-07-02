8e1dabefbf573986b88a9501930ec4a47aaa8dc9
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:33:37 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a[]=new int[5];
+int n=s.nextInt();
+for(int i=0;i<n;i++)
+a[i]=s.nextInt();
+for(int i=0;i<n;i++)
+  System.out.println(a[i]+ " ");
+}
+}
