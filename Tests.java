044bcc463593bbef61d6291f738aee6885b1aecc
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:33:54 +0530

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.String;
+class Tests
+{
+public static void main(String[]  args)
+{
+String s1="java lang";
+System.out.println(s1);
+char s2[]=s1.toCharArray();
+System.out.println(Arrays.toString(s2));
+for(int i=0;i<s2.length;i++)
+{
+System.out.println(s2[i]);
+}
+}
+}
