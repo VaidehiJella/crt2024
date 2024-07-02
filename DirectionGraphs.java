dd749023c5d7db33930321fdca373feb07b16482
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:25:55 +0530

    Add files via upload

---
+++ DirectionGraphs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.String;
+class DirectionGraphs
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+   String str="Java is simple language";
+   System.out.println(str.contains("simple"));
+   System.out.println(str.contains("python"));
+   System.out.println(str.contains("lan"));
+   System.out.println(str.startsWith("J"));
+   System.out.println(str.startsWith("A"));
+   System.out.println(str.endsWith("age"));
+    
+  }
+}
