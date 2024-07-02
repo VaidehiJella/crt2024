9550b9c902de9705e12ce0225fd7d67ec0993018
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:32:17 +0530

    Add files via upload

---
+++ StringSort.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.String;
+class StringConcatenate
+{
+public static void main(String[]  args)
+{
+String str1="satish";
+String str2="soft";
+String str3=str1+str2;
+System.out.println(str3);
+}
+}
+
