ffe18902f7432ee666899e48c837a53b24305d6d
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:25:09 +0530

    Add files via upload

---
+++ Constanttime.java
@@ -0,0 +1,15 @@
+class Lineartime
+{
+int c=0;
+for(int i=0;i<n;i++)
+{
+c++;
+}
+return c;
+}
+ public static void main(String args[])
+{
+ int n=100;
+System.out.println("n=100, no of instructions o(n):" +f(n);
+}
+}
