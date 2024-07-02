08ce087d5b2dcf73effffcaff965dbe28bc95c2b
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:32:38 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.String;
+class  SubString
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+   String str1=s.nextLine();
+   String str2=s.nextLine();
+   if(str1.contains(str2))
+   {
+     System.out.println("String is found");
+   }
+   else
+   {
+     System.out.println("String is not found");
+   }
+  }
+}
