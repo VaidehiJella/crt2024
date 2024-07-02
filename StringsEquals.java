dee2ed79cd074b45b06ffe305f4e58647318de01
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:31:58 +0530

    Add files via upload

---
+++ StringsEquals.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.String;
+class StringsEquals
+{
+public static void main(String[]  args)
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2=s.next();
+if(s1.equals(s2))
+{
+System.out.println("Same");
+}
+else
+{
+System.out.println("Not same");
+}
+}
+}
