d6b05b4327f2692a4100b87c6bd2fe0e511eddbd
VaidehiJella <166936858+VaidehiJella@users.noreply.github.com>
2024-07-02 21:30:19 +0530

    Add files via upload

---
+++ StringConcatenate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.String;
+class StringConcatenate
+{
+public static void main(String[]  args)
+{
+String str="satish";
+String str1=str.concat("soft");
+System.out.println(str);
+System.out.println(str1);
+}
+}
+
